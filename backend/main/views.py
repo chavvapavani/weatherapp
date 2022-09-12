f37d888975c8213b00d3424935ae96c3f96ca733
Chavva Pavani <pavani.k@prodapt.com>
2022-09-12 21:03:15 +0530

    getting weather data for the given city as input

--- backend/main/views.py
+++ backend/main/views.py
@@ -1,12 +1,12 @@
 from rest_framework.response import Response
 import requests
 from rest_framework.views import APIView
-class Api(APIView):
-    def get(self,request,format=None):
-        url="http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=b0b2a8c6eb68cf455d2b353ed0537b55"
+class Api(APIView):  
+    def get(self,request,format=None): 
+        city=request.data.get('city') #getting the city name as input by the user
+        url=f"http://api.openweathermap.org/data/2.5/weather?q={city}&APPID=b0b2a8c6eb68cf455d2b353ed0537b55"
         data=requests.get(url)   #requesting the given url for the data
         json_data=data.json()  #converting the data into json format
-        print(json_data)
         return Response(json_data) 
 
 
