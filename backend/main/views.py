87c9f4ffad9b6e84fe5dbf51930485278c99e721
Chavva Pavani <pavani.k@prodapt.com>
2022-09-12 21:39:03 +0530

    updated get method to filter the json data for the required fields

--- backend/main/views.py
+++ backend/main/views.py
@@ -7,6 +7,16 @@ class Api(APIView):
         url=f"http://api.openweathermap.org/data/2.5/weather?q={city}&APPID=b0b2a8c6eb68cf455d2b353ed0537b55"
         data=requests.get(url)   #requesting the given url for the data
         json_data=data.json()  #converting the data into json format
-        return Response(json_data) 
+        #getting only required fields from json data
+        response_data = {
+            "weather_condition": json_data['weather'][0]['description'],
+            "icon": json_data['weather'][0]['icon'],
+            "temperature": json_data['main']['temp'],
+            "city": json_data['name'],
+            "humidity":json_data['main']['humidity'],
+            "pressure":json_data['main']['pressure']
+        }
+        return Response(response_data)
+       
 
 
