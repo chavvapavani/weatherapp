7d435c32467986eee7cf17dcba153a10f0df23bf
Chavva Pavani <pavani.k@prodapt.com>
2022-09-14 18:23:51 +0530

    implemented post method to fetch 5 day forecast data for the given city

--- backend/main/views.py
+++ backend/main/views.py
@@ -24,6 +24,28 @@ class WeatherApi(APIView):
             "pressure":json_data['main']['pressure'],
         }
         return Response(response_data)
+class WeatherForecast(APIView):
+    serializer_class=WeatherAPiSerializer
+    def post(self,request): 
+        serializer=self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        city=serializer.validated_data.get('city')
+        url=f"http://api.openweathermap.org/data/2.5/forecast?q={city}&units=metric&APPID=b0b2a8c6eb68cf455d2b353ed0537b55"
+        data=requests.get(url)   
+        json_data=data.json()
+        date_list=[]
+        forecast_data={}
+        for i in range(0,json_data['cnt']):
+                date=json_data['list'][i]['dt_txt'].split(" ")[0]
+                if date not in date_list:
+                    date_list.append(date)
+                    each_date={}
+                    each_date.update({"temperature":json_data['list'][i]['main']['temp']})
+                    each_date.update({"descriptrion":json_data['list'][i]['weather'][0]['description']})
+                    each_date.update({"icon":json_data['list'][i]['weather'][0]['icon']})
+                    each_date.update({"date":date})
+                    forecast_data.update({date:each_date})
+        return Response(forecast_data)
 
 
 
