a6e92fbb55f1850806bb12782af70840d6d5d4cb
Chavva Pavani <pavani.k@prodapt.com>
2022-09-15 23:27:31 +0530

    search bar is designed,added logo and designed template to display current weather data

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+  displaycity:String="";
+  city:String="" ;
+  isDisplay = true;
+  toggleDisplay(){
+    if(this.city!="")
+    {
+    this.isDisplay = false;
+    this.displaycity=this.city;
+    }
+    else{
+      this.isDisplay=true;
+    }
+
+  }
+
+
+
 }
